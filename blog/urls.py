4f010d88c809334707e632187ce6b3ed23b8d853
zamknijryj <zamknijryj.x@tutanota.com>
2017-09-03 11:03:09 +0200

    Added post detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.urls import url
 from . import views
+from .views import PostList, PostDetail
 
 urlpatterns = [
-    url(r'^$', views.blog, name='blog'),
+    url(r'^$', PostList.as_view(), name='blog'),
+    url(r'(?P<pk>[^/]+)', PostDetail.as_view(), name='post'),
+    url(r'(?P<pk>[^/]+)/(?P<slug>[-\w]+)$',
+        PostDetail.as_view(), name='post_detail'),
 ]
